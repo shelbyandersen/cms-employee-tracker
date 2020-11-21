566d8fead69fe70b43e9f9c067d406690cd668a5
Shelby <shelbyrobison14@gmail.com>
2020-11-21 17:02:54 -0500

    created seed file for sql

---
+++ seed.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS cms_employeeDB;
+
+CREATE DATABASE cms_employeeDB;
+
+USE cms_employeeDB;
+-- - **department**:
+--   - **id** - INT PRIMARY KEY
+--   - **name** - VARCHAR(30) to hold department name
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+-- - **role**:
+--   - **id** - INT PRIMARY KEY
+--   - **title** - VARCHAR(30) to hold role title
+--   - **salary** - DECIMAL to hold role salary
+--   - **department_id** - INT to hold reference to department role belongs to
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary INT NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+-- - **employee**:
+--   - **id** - INT PRIMARY KEY
+--   - **first_name** - VARCHAR(30) to hold employee first name
+--   - **last_name** - VARCHAR(30) to hold employee last name
+--   - **role_id** - INT to hold reference to role employee has
+--   - **manager_id** - INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name INT NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY(id)
+);
