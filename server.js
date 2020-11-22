c870f49140a56aa33a9749372a11741db94a8f91
Shelby <shelbyrobison14@gmail.com>
2020-11-21 19:31:58 -0500

    required mysql and inquirer, began connection

---
+++ server.js
@@ -0,0 +1,59 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "Running123",
+  database: "cms_employeeDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  userInput();
+});
+
+function userInput() {
+//   inquirer
+//     .prompt({
+//       name: "action",
+//       type: "list",
+//       message: "What would you like to do?",
+//       choices: [
+//         "Find songs by artist",
+//         "Find all artists who appear more than once",
+//         "Find data within a specific range",
+//         "Search for a specific song",
+//         "exit"
+//       ]
+//     })
+//     .then(function(answer) {
+//       switch (answer.action) {
+//       case "Find songs by artist":
+//         artistSearch();
+//         break;
+
+//       case "Find all artists who appear more than once":
+//         multiSearch();
+//         break;
+
+//       case "Find data within a specific range":
+//         rangeSearch();
+//         break;
+
+//       case "Search for a specific song":
+//         songSearch();
+//         break;
+
+//       case "exit":
+//         connection.end();
+//         break;
+//       }
+//     });
+}
+
+userInput();
