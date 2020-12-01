bd3e26bcec8d237b0417c091e3dbbe9776dc145b
Shelby <shelbyrobison14@gmail.com>
2020-12-01 15:27:01 -0500

    prompt user with employee choices, get manager function from table

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // require
 const mysql = require("mysql");
-const prompt = require("inquirer");
+const inquirer = require("inquirer");
 const cTable = require("console.table")
 let Database = require("./lib/db");
 
 // create db connection
-const connection = mysql.createConnection({
+const db = new Database({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -13,48 +13,75 @@ const connection = mysql.createConnection({
   database: "cms_employeeDB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-//   userInput();
-});
-
-function userInput() {
-//   inquirer
-//     .prompt({
-//       name: "action",
-//       type: "list",
-//       message: "What would you like to do?",
-//       choices: [
-//         "Find songs by artist",
-//         "Find all artists who appear more than once",
-//         "Find data within a specific range",
-//         "Search for a specific song",
-//         "exit"
-//       ]
-//     })
-//     .then(function(answer) {
-//       switch (answer.action) {
-//       case "Find songs by artist":
-//         artistSearch();
-//         break;
-
-//       case "Find all artists who appear more than once":
-//         multiSearch();
-//         break;
-
-//       case "Find data within a specific range":
-//         rangeSearch();
-//         break;
-
-//       case "Search for a specific song":
-//         songSearch();
-//         break;
-
-//       case "exit":
-//         connection.end();
-//         break;
-//       }
-//     });
+// determine who is a manager from our employee table.
+async function getManagerName(){
+  let query = "SELECT * FROM employee WHERE manager_id IS NULL";
+  // loop through employee table
+  const rows = await db.query(query);
+  let employeeNames = [];
+  for (const employee of rows){
+      employeeNames.push(employee.first_name + " " + employee.last_name);
+  }
+  return employeeNames;
 }
 
-userInput();
+// ask user what they want to do
+async function start() {
+  return inquirer
+  .prompt([
+      {
+          type:"list",
+          message:"What would you like to do?",
+          name:"actions",
+          choices:[
+          "Add department", 
+          "Add role", 
+          "Add employee",
+          "View departments", 
+          "view roles", 
+          "view employees",
+          "Update employee roles",
+          "Delete employee",
+          "view all employees by department",
+          "view employees by manager",
+          "EXIT"]
+      }
+  ]);
+}
+
+// prompt the user to get the employee information
+async function getAddEmployeeInfo(){
+  const managers = await getManagerName();
+  const roles = await getRoles();
+  return inquirer.prompt([
+      {
+          
+          name:"first_name",
+          type:"input",
+          message:"What is employee's first name?"
+      },
+      {
+          name:"last_name",
+          type:"input",
+          message:"What is employee's last name?"   
+      },
+      {
+          name:"role",
+          type:"list",
+          message:"What is the employee's role?",
+          choices: [
+              // we can use spread operator to pass the values of our roles array populate from the database.
+              ...roles
+          ]
+      },
+      {
+          name:"manager",
+          type:"list",
+          message:"Who is the employee's manager?",
+          choices: [
+              // we can use spread operator to pass the values of our managers array populate from database
+              ...managers
+          ]
+      }
+  ]);
+}
